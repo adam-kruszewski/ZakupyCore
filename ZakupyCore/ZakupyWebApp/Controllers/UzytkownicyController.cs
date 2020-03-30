a203979dfd81d0ff5a12c0d4725d3ab495bf8376
Adam Kruszewski <kruchy.jr@wp.pl>
2020-03-30 19:56:19 +0200

    Wyświetlanie listy użytkowników

---
+++ ZakupyCore/ZakupyWebApp/Controllers/UzytkownicyController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ZakupyWebApp.Models;
+
+namespace ZakupyWebApp.Controllers
+{
+    public class UzytkownicyController : Controller
+    {
+
+
+        public ActionResult Index()
+        {
+            var model = new ListaUzytkownikowModel();
+
+            model.Uzytkownicy = new[]
+            { 
+                new UzytkownikRowModel()
+                {
+                    Nazwa = "Adam",
+                    Email = "adam@adam.pl",
+                    ID = 1983
+                }
+            }
+                    .ToList();
+
+            return View(model);
+        }
+    }
+}
---
+++ ZakupyCore/ZakupyWebApp/Models/ListaUzytkownikowModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace ZakupyWebApp.Models
+{
+    public class ListaUzytkownikowModel
+    {
+        public List<UzytkownikRowModel> Uzytkownicy { get; set; }
+
+        public ListaUzytkownikowModel()
+        {
+            Uzytkownicy = new List<UzytkownikRowModel>();
+        }
+    }
+}
