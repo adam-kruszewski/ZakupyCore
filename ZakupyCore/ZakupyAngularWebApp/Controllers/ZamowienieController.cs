c610005ce4a11507195a96786e9db4ecb4c2f6d9
kruchy <kruchy@piatka.pl>
2020-04-15 10:20:45 +0200

    Controller do poobieranie danych definicji zamówienia

---
+++ ZakupyCore/ZakupyAngularWebApp/Controllers/ZamowienieController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZakupyAngularWebApp.Controllers
+{
+    [Route("api/zamowienie")]
+    public class ZamowienieController : Controller
+    {
+        [HttpGet]
+        public DefinicjaZamowienia DajWgID(int id)
+        {
+            return new DefinicjaZamowienia
+            {
+                ID = id,
+                Nazwa = "Zamówienie do szczegółów",
+                DataKonca = DateTime.Now.AddDays(2)
+            };
+        }
+    }
+}
