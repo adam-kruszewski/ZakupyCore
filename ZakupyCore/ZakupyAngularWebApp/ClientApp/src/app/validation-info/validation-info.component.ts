07806426347b607ccfbce2cf8616571ee467f043
kruchy <kruchy@piatka.pl>
2020-04-28 11:42:58 +0200

    walidacja i zgrubsza ustawienie kontrolek na formularzu

--- ZakupyCore/ZakupyAngularWebApp/ClientApp/src/app/validation-info/validation-info.component.ts
+++ ZakupyCore/ZakupyAngularWebApp/ClientApp/src/app/validation-info/validation-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-validation-info',
@@ -9,11 +10,24 @@ export class ValidationInfoComponent implements OnInit {
 
   @Input() control: any;
   @Input() controlLabel: string;
+  @Input() validationErrorsFunctions: ValidationError[] = []
 
   constructor() { }
 
   ngOnInit() {
-    window.alert('validation on init');
+    console.log('Liczba funkcji walidatorów: ' + this.validationErrorsFunctions.length);
   }
 
+}
+
+export class ValidationError {
+  text: string;
+  checkDisplayFunction: (control: AbstractControl) => boolean;
+
+  constructor(
+    text: string,
+    checkDisplayFunction: (AbstractControl) => boolean) {
+    this.text = text;
+    this.checkDisplayFunction = checkDisplayFunction;
+  }
 }
